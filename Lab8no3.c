b159d8dd140f3676050207c852aadd7ff7b964fa
PAPHAWIN KOKARAT <s6804062612366@email.kmutnb.ac.th>
2026-02-23 00:16:10 +0700

    Commit 6 >> Lab8no3.c: Sol2 use binary search

--- Lab8no3.c
+++ Lab8no3.c
@@ -5,6 +5,7 @@ int random1to100();
 void printAr(int numAr[],int numArSize);
 int checkNum(int numAr[],int numArSize);
 int searchNum(int numAr[],int numArSize,int target);
+void bubbleSort(int numAr[], int numArSize);
 
 int main() {
     int N,i;
@@ -15,6 +16,7 @@ int main() {
     srand(time(NULL));
     for (i=0;i<N;i++)
         A[i]=checkNum(A,i);
+        bubbleSort(A, i + 1);
        
     printf("\n********\n");
     printAr(A,N);
@@ -37,14 +39,34 @@ int checkNum(int numAr[],int numArSize) {
 }
 
 int searchNum(int numAr[],int numArSize, int target) {
-        int i,found=0;
-        for (i = 0;i < numArSize; i++) {
-            if (numAr[i] == target) {
+        int left = 0,right = numArSize - 1;
+        int mid,found = 0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (numAr[mid] == target) {
                 found = 1;
                 break;
             }
+            if (numAr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
         return found;
+}
+
+void bubbleSort(int numAr[], int numArSize) {
+    int temp;
+    for (int i = 0; i < numArSize - 1; i++) {
+        for (int j = 0; j < numArSize - i - 1; j++) {
+            if (numAr[j] > numAr[j + 1]) {
+                temp = numAr[j];
+                numAr[j] = numAr[j + 1];
+                numAr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 int random1to100() {
